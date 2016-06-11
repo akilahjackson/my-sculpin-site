ed083c867cbd68f412b5bcd62f2e873c0e6fc300
Akilah Jackson <akilah@mindmapengineers.com>
2016-06-11 11:58:58 -0400

    commented out the part of the script that comes with publish.sh

--- publish.sh
+++ publish.sh
@@ -4,12 +4,12 @@
 # was downloaded and placed in this directory instead of sculpin having been
 # installed globally.
 
-sculpin generate --env=prod
-if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
+#sculpin generate --env=prod
+#if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
 # Add --delete right before "output_prod" to have rsync remove files that are
 # deleted locally from the destination too. See README.md for an example.
-rsync -avze 'ssh -p 4668' output_prod/ akilahjackson@my-sculpin-site:public_html
+#rsync -avze 'ssh -p 4668' output_prod/ akilahjackson@my-sculpin-site:public_html
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
 
 if [ $# -ne 1 ]; then
